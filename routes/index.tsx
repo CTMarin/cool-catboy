e2f1dfc1b47c50aa7b507e1372972238dedbfbd2
carlos <carlomaco11@gmail.com>
2022-09-09 18:23:06 +0200

    fix: footer display

--- routes/index.tsx
+++ routes/index.tsx
@@ -18,11 +18,14 @@ export default function Home() {
 					/>
                 </section>
             </main>
-			<footer class="w-11/12 h-16 flex items-center justify-between">
-				<a href="https://fresh.deno.dev">
+			<footer class="w-full h-16 px-8 flex items-center justify-between">
+				<a href="https://fresh.deno.dev" target="_blank" rel="noopener noreferrer">
 					<img width="197" height="37" src="https://fresh.deno.dev/fresh-badge-dark.svg" alt="Made with Fresh" />
 				</a>
-				<a href="https://github.com">
+				<span class="font-bold text-gray-400">
+					More features coming soon...
+				</span>
+				<a class="w-48 flex items-center justify-center" href="https://github.com" target="_blank" rel="noopener noreferrer">
 					<img width="37" height="37" src="github-icon.png" alt="Link to source code on Github" />
 				</a>				
 			</footer>
