f317c61ca4f8e85234e78aff0cc49a6cd1e52fbb
carlos <carlomaco11@gmail.com>
2022-09-09 18:05:55 +0200

    new: added footer

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,6 @@
 export default function Header() {
     return (
-        <header class="">
+        <header class="w-full">
             <h1 class="h-16 bg-purple-500 w-full text-center text-5xl font-extrabold text-gray-900">
                 Cool
                 <span class="text-white"> Catboys</span>
--- routes/index.tsx
+++ routes/index.tsx
@@ -4,9 +4,9 @@ import LinkButton from "../components/LinkButton.tsx";
 
 export default function Home() {
     return (
-        <div class="h-full">
+        <div class="h-full flex flex-col items-center">
             <Header/>
-            <main class="flex flex-col h-[calc(100%-4rem)] justify-center items-center">
+            <main class="flex flex-col w-full h-[calc(100%-4rem-4rem)] justify-center items-center">
                 <section class="flex flex-col items-center">
                     <h2 
                         class="text-3xl font-bold mb-8">
@@ -18,6 +18,14 @@ export default function Home() {
 					/>
                 </section>
             </main>
+			<footer class="w-11/12 h-16 flex items-center justify-between">
+				<a href="https://fresh.deno.dev">
+					<img width="197" height="37" src="https://fresh.deno.dev/fresh-badge-dark.svg" alt="Made with Fresh" />
+				</a>
+				<a href="https://github.com">
+					<img width="37" height="37" src="github-icon.png" alt="Link to source code on Github" />
+				</a>				
+			</footer>
         </div>
     );
 }
