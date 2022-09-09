08da940acbed0a472d2a8a8306d65d7a515e1ed2
carlos <carlomaco11@gmail.com>
2022-09-09 23:19:53 +0200

    fix: minor changes on header style

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,7 @@
 export default function Header() {
     return (
-        <header class="w-full">
-            <h1 class="h-16 bg-purple-500 w-full text-center text-5xl font-extrabold text-gray-900">
+        <header class="w-full bg-purple-500">
+            <h1 class="h-16 mt-2 text-center text-5xl font-extrabold text-gray-900">
                 Cool
                 <span class="text-white"> Catboys</span>
             </h1>
